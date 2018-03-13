cf455970a75b174fbb5fa97c77d748652f52bafa
Tim Burnett <toburnett@xtra.com>
2018-03-13 15:10:17 -0500

    Added translations to spanish.

--- postgres scripts/Translations.sql
+++ postgres scripts/Translations.sql
@@ -3,14 +3,23 @@ INSERT INTO cultures ("code", "name") VALUES
 ('es-mx', 'Español')
 
 
-INSERT INTO translations ("dtmCreated", "keyCultureId", "key", "dtmUpdated", "tranCultureId", "value") VALUES
-(now(), 1, 'Household Name', now(), 2, 'Familia'),
-(now(), 1, 'Home Phone', now(), 2, 'Teléfono de casa'),
-(now(), 1, 'Street Address', now(), 2, 'Dirección'),
-(now(), 1, 'City', now(), 2, 'Ciudad'),
-(now(), 1, 'State', now(), 2, 'Estado'),
-(now(), 1, 'Zip', now(), 2, 'Codigo'),
-(now(), 1, 'Adult 1', now(), 2, 'Adulto 1'),
-(now(), 1, 'Adult 2', now(), 2, 'Adulto 2'),
-(now(), 1, 'Email Address', now(), 2, 'correo electronico'),
-(now(), 1, 'Mobile Phone', now(), 2, 'Teléfono móvil')
+INSERT INTO translations ("dtmCreated", "dtmUpdated", "keyCultureId", "tranCultureId", "key", "value") VALUES
+(now(), now(), 1, 2, 'Borromeo Parish Directory Signup 2018', 'Registro 2018 de la Parroquia Borromeo'),
+(now(), now(), 1, 2, 'This is the sign up form for the Parish Directory Update', 'Este es el formulario de registro para la actualizacion del Directorio Parroquial'),
+(now(), now(), 1, 2, 'Household Name', 'Nombre Familiar'),
+(now(), now(), 1, 2, 'Home Phone', 'Teléfono de casa'),
+(now(), now(), 1, 2, 'Street Address', 'Dirección'),
+(now(), now(), 1, 2, 'City', 'Ciudad'),
+(now(), now(), 1, 2, 'State', 'Estado'),
+(now(), now(), 1, 2, 'Zip', 'Codigo'),
+(now(), now(), 1, 2, 'Publish phone number and address in directory?', 'Publicar el numero de telefono y su direccion en el directorio? '),
+(now(), now(), 1, 2, 'i.e. Smith', 'ej. Smith'),
+(now(), now(), 1, 2, 'i.e. 636-946-1893', 'ej. 636-946-1893'),
+(now(), now(), 1, 2, 'Adult 1', 'Adulto 1'),
+(now(), now(), 1, 2, 'Adult 2', 'Adulto 2'),
+(now(), now(), 1, 2, 'Email Address', 'correo electronico'),
+(now(), now(), 1, 2, 'test@test.com', 'ejemplo@ejemplo.com'),
+(now(), now(), 1, 2, 'Mobile Phone', 'Teléfono móvil'),
+(now(), now(), 1, 2, 'Children and other Familiy Members', 'Hijos y otros familiares'),
+(now(), now(), 1, 2, 'Enter Children and others seperated by commas', 'Escribir nombres de hijos y familiares separados por comas.'),
+(now(), now(), 1, 2, 'Submit Directory Form', 'Enviar Formulario')
