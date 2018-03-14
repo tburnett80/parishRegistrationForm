a038cdecd99659bf837323b091041bd30ed32831
Tim <tburnett80@gmail.com>
2018-03-13 23:06:48 -0500

    Added container build script

---
+++ bash scripts/build_img.sh
@@ -0,0 +1,7 @@
+
+git clone https://github.com/tburnett80/parishRegistrationForm.git
+cd  parishRegistrationForm/ParishForms/ParishForms
+dotnet publish -c Release -o out /p:CopyOutputSymbolsToPublishDirectory=false
+docker build -t parish-frm .
+
+rm -rf parishRegistrationForm/
